7206a4166532d5f32e1148fed1176ec53ac741e7
Kovidh <kovidhvsbhati@gmail.com>
2025-12-03 23:06:52 +0530

    add comment

--- src/tcp/clientSession.cpp
+++ src/tcp/clientSession.cpp
@@ -16,12 +16,15 @@ std::string ClientSession::recvFromClient(){
             buff.resize(buff.size()*2);
         }
 
+        // call recv(for client's socket, buff starting mem addr, available space that can be filled, remote entity closes the connection)
         bytesRecv = recv(
             this->socket, 
             buff.data() + totalBytesRecv, 
             buff.size() - totalBytesRecv, 
             0
         );
+
+        // check if all data received 
         if(bytesRecv > 0){
             totalBytesRecv += bytesRecv;
         } else if(bytesRecv == 0) {
@@ -31,5 +34,6 @@ std::string ClientSession::recvFromClient(){
             break;
         }
     }
+    
     return std::string(buff.data(), totalBytesRecv);
 }
