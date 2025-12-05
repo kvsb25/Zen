27a55bebaa873ac1bd5c71b2d4c28d4e87329581
Kovidh <kovidhvsbhati@gmail.com>
2025-12-05 21:04:31 +0530

    define function void log(string); define function void exitWithError(string);

--- include/zen/tcp/tcpServer.hpp
+++ include/zen/tcp/tcpServer.hpp
@@ -3,6 +3,16 @@
 #include<iostream>
 #include<winsock2.h>
 
+void log(const std::string& message){
+    std::cout << message << std::endl;
+}
+
+void exitWithError(const std::string& errorMessage){
+    std::cerr << WSAGetLastError() << std::endl;
+    log("ERROR: " + errorMessage);
+    exit(1);
+}
+
 class TcpServer{
 private:
     SOCKET main_socket = INVALID_SOCKET;
