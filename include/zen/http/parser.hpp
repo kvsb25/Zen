666496f75003f2403faae751bae5fec04a54d0c4
Kovidh <kovidhvsbhati@gmail.com>
2025-12-06 22:08:58 +0530

    complete parser header file

--- include/zen/http/parser.hpp
+++ include/zen/http/parser.hpp
@@ -2,3 +2,10 @@
 #include "utils.hpp"
 #include "response.hpp"
 #include "request.hpp"
+
+namespace http{
+    class Parser{
+        static Request* parse(const std::string& req);
+        static std::string construct(const Response* &res);
+    };
+}
