f119937d9187585a514a13c8115b89dc26d03acf
Kovidh <kovidhvsbhati@gmail.com>
2025-12-06 22:04:05 +0530

    complete request header file

---
+++ include/zen/http/request.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "utils.hpp"
+
+namespace http{
+    struct Request{
+        std::string method;
+        std::string path;
+        std::unordered_map<std::string, std::string> query;
+        std::unordered_map<std::string, std::string> params;
+        std::string version;
+        std::unordered_map<std::string, std::string> headers;
+        std::string body;
+    };
+}
