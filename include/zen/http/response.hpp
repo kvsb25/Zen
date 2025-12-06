573ae8296436411ec70613eea63da70e5e07d398
Kovidh <kovidhvsbhati@gmail.com>
2025-12-06 22:03:19 +0530

    complete response header file

---
+++ include/zen/http/response.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "utils.hpp"
+#include <vector>
+
+namespace http{
+    struct Response{
+        int status_code = 200;
+        std::string status_message = "OK";
+        std::unordered_map<std::string, std::string> headers;
+        std::string body;
+
+        Response& send(std::string& data);
+        Response& status(int status);
+        Response& header(std::pair<std::string, std::string>& key_value_pair);
+        Response& header(std::vector<std::pair<std::string, std::string>>& headers);
+        Response& redirect(std::string& absolute_url);
+    };
+}
