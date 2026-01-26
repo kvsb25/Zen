d6fd637fed036af75a596c40953d304de0da527f
Kovidh <kovidhvsbhati@gmail.com>
2026-01-26 15:20:52 +0530

    default middleware source file

--- src/middleware/defaultMiddleware.cpp
+++ src/middleware/defaultMiddleware.cpp
@@ -1,9 +1,5 @@
 #include "../../include/zen/middleware/defaultMiddleware.hpp"
 
 namespace middleware{
-    class DefaultMiddleware : public Middleware{
-    public:
-        std::function<void(http::Request& req, http::Response& res)> handler;
-        DefaultMiddleware(std::function<void(http::Request& req, http::Response& res)> callback) : Middleware(Type::DEFAULT), handler(callback){};
-    };
+    DefaultMiddleware::DefaultMiddleware(std::function<void(http::Request& req, http::Response& res)> callback) : Middleware(Type::DEFAULT), handler(callback){};
 }
