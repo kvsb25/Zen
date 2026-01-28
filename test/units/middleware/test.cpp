03cae92d67989ba1ed70665a36823c152943d01b
Kovidh <kovidhvsbhati@gmail.com>
2026-01-28 23:37:39 +0530

    middleware test

--- test/units/middleware/test.cpp
+++ test/units/middleware/test.cpp
@@ -2,7 +2,19 @@
 
 int main(){
 
-    middleware::PathMiddleware pm("GET", "/", [](http::Request req, http::Response res){
+    std::string req_string =
+        "GET / HTTP/1.1\r\n"
+        "Host: localhost:3000\r\n"
+        "User-Agent: MicroFrameworkTest/1.0\r\n"
+        "Accept: application/json\r\n"
+        "Connection: keep-alive\r\n"
+        "\r\n";
+    // http::Request* req = new http::Request(req_string);
+    http::Request req(req_string);
+    // http::Response* res = new http::Response();
+    http::Response res;
+
+    middleware::Middleware* pm = new middleware::PathMiddleware("GET", "/", [](http::Request req, http::Response res){
         std::cout<< "req.method: " << req.method << std::endl 
             << "req.path: " << req.path << std::endl
             << "req.body: " << req.body << std::endl;
@@ -11,12 +23,22 @@ int main(){
     });
 
     // std::vector<std::unique_ptr<middleware::Middleware>> pipe;
-    std::vector<middleware::Middleware> pipe;
+
+    std::vector<middleware::Middleware*> pipe;
     pipe.push_back(pm);
 
-    // for(auto m : pipe){
-    //     middleware::Middleware m = 
-    // }
+    for(auto m : pipe){
+        if(m->type == middleware::Type::DEFAULT){
+            middleware::DefaultMiddleware* dm = static_cast<middleware::DefaultMiddleware*>(m);
+            dm->handler(req, res);
+        } else if(m->type == middleware::Type::PATH){
+            middleware::PathMiddleware* pm = static_cast<middleware::PathMiddleware*>(m);
+            if(pm->match(req)){
+                pm->handler(req, res);
+                break;
+            }
+        }
+    }
 
     return 0;
 }
