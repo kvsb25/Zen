5deb9b62229434e1dcd9f9d2f486613b2e0b5fce
Kovidh <kovidhvsbhati@gmail.com>
2026-02-21 21:04:24 +0530

    test Error handling Middleware

--- test/units/middleware/test.cpp
+++ test/units/middleware/test.cpp
@@ -1,9 +1,58 @@
 #include "tester.hpp"
 
+void testHandle(http::Request& req, http::Response& res, std::vector<middleware::Middleware*> pipe, int index = 0, const ZenErr& err = ZenErr("", false)){
+    
+    if(index >= pipe.size()) return;
+
+    middleware::Middleware* mw = pipe[index]; // create a borrowed raw pointer of the unique_ptr at 'index' in pipe // DON'T DELETE mw MANUALLY
+    try
+    {
+        if (!err.isRelevant())
+        {
+            std::cout << "Error is not relevant!" << std::endl;
+            if (mw->type == middleware::Type::DEFAULT)
+            {
+                auto *dmw = static_cast<middleware::DefaultMiddleware *>(mw); // down cast
+                dmw->handler(req, res);
+                // give flag -02 or -03 with gcc to leverage tail call optimization
+            }
+            else if (mw->type == middleware::Type::PATH)
+            {
+                auto *pmw = static_cast<middleware::PathMiddleware *>(mw); // down cast
+                if (pmw->match(req))
+                {
+                    pmw->handler(req, res);
+                    return;
+                }
+            }
+        }
+        else
+        {
+            if (mw->type == middleware::Type::ERR)
+            {
+                auto *emw = static_cast<middleware::ErrorMiddleware *>(mw);
+                emw->handler(err, req, res);
+            }
+        }
+        return testHandle(req, res, pipe, index + 1, err);
+    }
+    catch (const ZenErr &e)
+    {
+        if(e.isRelevant())
+            return testHandle(req, res, pipe, index + 1, e);
+    }
+    catch (const std::runtime_error& e){
+        throw;
+    }
+    // delete all pointers on error 
+
+    return;
+}
+
 int main(){
 
     std::string req_string =
-        "GET /vendor/product/1234 HTTP/1.1\r\n"
+        "GET / HTTP/1.1\r\n"
         "Host: localhost:3000\r\n"
         "User-Agent: MicroFrameworkTest/1.0\r\n"
         "Accept: application/json\r\n"
@@ -18,18 +67,23 @@ int main(){
     http::Response res;
 
     middleware::Middleware* emw = new middleware::ErrorMiddleware([](const ZenErr& e, http::Request& req, http::Response& res){
-        std::cout << e.what() << std::endl;
+        // std::cout << e.what() << std::endl;
+        res.initErrRes(404, "testing");
         // std::cout << e.getTrace() << std::endl;
+        std::cout << " in error middleware: TEST: constructed response: " << res.construct() << std::endl;
         return;
     });
 
     // make sure the request and response objects sent to the handler are references
     middleware::Middleware* pm = new middleware::PathMiddleware("GET", "/", [](http::Request& req, http::Response& res){
+        if(true){
+            throw ZenErr("Test error");
+        }
+        
         std::cout<< "req.method: " << req.method << std::endl 
             << "req.path: " << req.path << std::endl
             << "req.body: " << req.body << std::endl;
         
-        if(true) throw ZenErr("Test error");
         
         res.json("{\"message\": \"hello\"}");
         return;
@@ -74,23 +128,26 @@ int main(){
     pipe.push_back(pm);
     pipe.push_back(pm2);
     pipe.push_back(dm);
+    pipe.push_back(emw);
 
-    for(auto m : pipe){
-        if(m->type == middleware::Type::DEFAULT){
-            middleware::DefaultMiddleware* dm = static_cast<middleware::DefaultMiddleware*>(m);
-            dm->handler(req, res);
-        } else if(m->type == middleware::Type::PATH){
-            middleware::PathMiddleware* pm = static_cast<middleware::PathMiddleware*>(m);
-            if(pm->match(req)){
-                pm->handler(req, res);
-                break;
-            } else {
-                std::cout << "Match result: " << pm->match(req) << std::endl;
-            }
-        }
-    }
+    testHandle(req, res, pipe);
 
-    std::cout << std::endl << "Final response string formed: " << std::endl << res.construct() << std::endl;
+    // for(auto m : pipe){
+    //     if(m->type == middleware::Type::DEFAULT){
+    //         middleware::DefaultMiddleware* dm = static_cast<middleware::DefaultMiddleware*>(m);
+    //         dm->handler(req, res);
+    //     } else if(m->type == middleware::Type::PATH){
+    //         middleware::PathMiddleware* pm = static_cast<middleware::PathMiddleware*>(m);
+    //         if(pm->match(req)){
+    //             pm->handler(req, res);
+    //             break;
+    //         } else {
+    //             std::cout << "Match result: " << pm->match(req) << std::endl;
+    //         }
+    //     }
+    // }
+
+    std::cout << std::endl << "After tesHandle: Final response string formed: " << std::endl << res.construct() << std::endl;
 
     return 0;
 }
--- test/units/middleware/test.ps1
+++ test/units/middleware/test.ps1
@@ -1,2 +1,2 @@
-g++ -std=c++23 "../../../src/middleware/errorMiddleware.cpp" "../../../src/middleware/defaultMiddleware.cpp" "../../../src/middleware/pathMiddleware.cpp" "../../../src/http/request.cpp" "../../../src/http/response.cpp" "./test.cpp" "./tester.cpp" -o test -lstdc++_libbacktrace;
+g++ -std=c++23 "../../../src/middleware/errorMiddleware.cpp" "../../../src/middleware/defaultMiddleware.cpp" "../../../src/middleware/pathMiddleware.cpp" "../../../src/http/request.cpp" "../../../src/http/response.cpp" "./test.cpp" "./tester.cpp" -o test;
 if($?){.\test.exe};
