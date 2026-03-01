e2831dfadaff10f6b3827f982156d79887d4d7fa
Kovidh <kovidhvsbhati@gmail.com>
2026-03-01 21:34:36 +0530

    add std::move to prevent copying heap allocations of captured variables of passed lambda function

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -6,39 +6,6 @@ void Zen::handle(http::Request& req, http::Response& res, int index, const ZenEr
 
     middleware::Middleware* mw = pipe[index].get(); // create a borrowed raw pointer of the unique_ptr at 'index' in pipe // DON'T DELETE mw MANUALLY
 
-    // if(!err.isRelevant() && mw->type == middleware::Type::DEFAULT){
-    //     try{
-    //         auto* dmw = static_cast<middleware::DefaultMiddleware*>(mw);  // down cast
-    //         dmw->handler(req, res);
-    //         // give flag -02 or -03 with gcc to leverage tail call optimization
-    //         return handle(req, res, index+1, err);
-
-    //     } catch (const ZenErr& e){
-    //         return handle(req, res, index+1, e);
-
-    //     } catch (const std::runtime_error& err) {
-    //         /*CHANGE ERROR HANDLING FOR ONION ARCH*/ throw HandlerErr(err.what());
-    //     }
-    // } else if(!err.isRelevant() && mw->type == middleware::Type::PATH) {
-    //     try{
-    //         auto* pmw = static_cast<middleware::PathMiddleware*>(mw);    // down cast
-    //         if(pmw->match(req)){
-    //             pmw->handler(req, res);
-    //             return;
-    //         }
-    //     } catch (const ZenErr& e){
-    //         return handle(req, res, index+1, e);
-    //     } catch (const std::runtime_error& err){
-    //         /*CHANGE ERROR HANDLING FOR ONION ARCH*/ throw HandlerErr(err.what());
-    //     }
-    // } else if(err.isRelevant() && mw->type == middleware::Type::ERR){
-    //     try{
-    //         auto* emw = static_cast<middleware::ErrorMiddleware*>(mw);
-    //         emw->handler(err, req, res);
-    //     } catch (...) {
-    //         throw HandlerErr(err.what());
-    //     }
-    // }
     try
     {
         if (!err.isRelevant())
@@ -86,14 +53,14 @@ void Zen::handle(http::Request& req, http::Response& res, int index, const ZenEr
 Zen::Zen() {}
 
 Zen& Zen::use(std::function<void(http::Request&, http::Response&)> callback){
-    auto mw = std::make_unique<middleware::DefaultMiddleware>(callback);
-    pipe.push_back(std::move(mw));
+    auto mw = std::make_unique<middleware::DefaultMiddleware>(std::move(callback));
+    pipe.push_back(std::move(mw)); // std::move() is for transferring exclusive ownership to the pipe
     return *this;
 }
 
 Zen& Zen::use(std::string method, std::string path, std::function<void(http::Request&, http::Response&)> callback){
-    auto mw = std::make_unique<middleware::PathMiddleware>(method, path, callback);
-    pipe.push_back(std::move(mw));
+    auto mw = std::make_unique<middleware::PathMiddleware>(method, path, std::move(callback));
+    pipe.push_back(std::move(mw)); 
     return *this;
 }
 
